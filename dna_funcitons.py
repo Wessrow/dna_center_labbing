afcde17f0d2e5301adaf9f3eb08eb0dd65f87b1e
Gustav Larsson <gurra_gla@hotmail.com>
2021-07-03 18:00:49 +0200

    Finished helper funcitons and started building out SDK-funcitonallity

--- dna_funcitons.py
+++ dna_funcitons.py
@@ -35,7 +35,7 @@ class DNACHandler:
     def _get_token(self):
         """ Internal helper fucntion to handle authentication """
 
-        token_url = f"https://{self.host}//dna/system/api/v1/auth/token"
+        token_url = f"https://{self.host}/dna/system/api/v1/auth/token"
 
         response = requests.post(token_url,
                                 auth=HTTPBasicAuth(self.user, self.password),
@@ -43,14 +43,40 @@ class DNACHandler:
 
         return response
 
-    def _req(self):
+    def _req(self, resource, method="GET", payload=None):
         """ Internal helper function to construct requests """
+
+        token = self._get_token().json()["Token"]
+        base_url = f"https://{self.host}/dna/intent/api/v1/"
+        api_call_url = f"{base_url}{resource}"
 
         headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
-            "X-Auth-Token": self._get_token()
+            "X-Auth-Token": token
         }
+
+        response = requests.request(method=method,
+                                    headers=headers,
+                                    url=api_call_url,
+                                    data=json.dumps(payload),
+                                    verify=self.verify)
+
+        return response
+
+    def get_site_health(self):
+        """ Fetches health of all sites available """
+
+        site_health = self._req("site-health")
+
+        return site_health
+
+    def get_client_health(self):
+        """ Fetches health of all clients available """
+
+        client_health = self._req("client-health")
+
+        return client_health
 
 if __name__ == "__main__":
 
@@ -63,4 +89,4 @@ if __name__ == "__main__":
 
     dnac_instance = DNACHandler(dnac_host, dnac_user, dnac_password)
 
-    print(dnac_instance._get_token())
+    print(dnac_instance.get_client_health().status_code)
